dde997efaed4d87f4f75d54275f505f1a26b0af1
Boardy <charlieboardy@gmail.com>
2025-07-02 22:45:57 +0100

    Revert "feat: Track Netlify branch in Google Analytics"
    This reverts commit a658344e2dcbcf2d373762c5500c44a3521613d8.

--- components/layout.js
+++ components/layout.js
@@ -22,7 +22,7 @@ export default function Layout({ children, home }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-EMZVLSXEFN', { 'branch': process.env.NEXT_PUBLIC_BRANCH });
+            gtag('config', 'G-EMZVLSXEFN');
           `}
         </Script>
       </Head>
