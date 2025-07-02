171c2fb1742676c5c0366c396753ca1f49c688d4
Boardy <charlieboardy@gmail.com>
2025-07-02 21:59:05 +0100

    feat: Add Google Analytics tracking
    - Integrate Google Analytics (GA4) tracking code into layout component.
    - Enables tracking of website visitors, session duration, page views, and repeat readers.

--- components/layout.js
+++ components/layout.js
@@ -14,6 +14,15 @@ export default function Layout({ children, home }) {
           content="The latest news, reviews, and stories from the automotive world."
         />
         <meta name="og:title" content={siteTitle} />
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EMZVLSXEFN"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-EMZVLSXEFN');
+        </script>
       </Head>
       <header className={styles.header}>
         <div className={styles.headerContent}>
